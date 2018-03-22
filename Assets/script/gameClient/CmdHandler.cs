e012b247c5f854aa80bf00e807339b57606d07df
osblow <osblow.wang@gmail.com>
2018-03-22 08:12:09 +0800

    添加基本通信组件

---
+++ Assets/script/gameClient/CmdHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class CmdHandler
+{
+    
+
+    public static T GetProtoInstance<T>(byte[] data, int index)
+    {
+        T t = default(T);
+        ushort length = BitConverter.ToUInt16(data, index);
+        index += 2;
+
+        using (MemoryStream ms = new MemoryStream())
+        {
+            //将消息写入流中
+            ms.Write(data, index, length);
+            //将流的位置归0
+            ms.Position = 0;
+            //使用工具反序列化对象
+        }
+
+        return t;
+    }
+
+
+    public static T GetProtoInstanceChat<T>(byte[] data, int index)
+    {
+        T t = default(T);
+        int length = BitConverter.ToInt32(data, index);
+        index += 4;
+
+        using (MemoryStream ms = new MemoryStream())
+        {
+            //将消息写入流中
+            ms.Write(data, index, length);
+            //将流的位置归0
+            ms.Position = 0;
+            //使用工具反序列化对象
+            
+        }
+
+        return t;
+    }
+}
---
+++ Assets/script/gameClient/CmdRequest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+namespace Osblow.Game
+{
+    public class CmdRequest
+    {
+        static void Serialize(short cmd, byte[] sendData)
+        {
+            List<byte> data = new List<byte>();
+
+            byte head = 0x64;
+            short lengths = (short)sendData.Length;
+            ushort length = (ushort)sendData.Length;
+            byte tail = 0x64;
+
+            data.Add(head);
+            data.AddRange(BitConverter.GetBytes(cmd));
+            data.AddRange(BitConverter.GetBytes(length));
+            data.AddRange(sendData);
+            data.Add(tail);
+
+            //Osblow.App.Globals.SceneSingleton<SocketNetworkMng>().Send(data.ToArray());
+        }
+    }
+}
