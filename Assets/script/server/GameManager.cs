099a4e5de411f6b17a65a000bf6daded34fbad42
osblow <osblow.wang@gmail.com>
2018-04-09 19:30:16 +0800

    添加游戏进度管理器

---
+++ Assets/script/server/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Osblow.Net.Server
+{
+    class GameManager : ObjectBase
+    {
+        private Dictionary<int, Player> m_players;
+
+
+        public override void Start()
+        {
+            base.Start();
+
+            m_players = new Dictionary<int, Player>();
+        }
+
+        public void AddPlayer(int guid, Player player)
+        {
+            if (m_players.ContainsKey(guid))
+            {
+                m_players[guid].Close();
+                m_players[guid].Destroy();
+            }
+
+            m_players[guid] = player;
+        }
+    }
+}
